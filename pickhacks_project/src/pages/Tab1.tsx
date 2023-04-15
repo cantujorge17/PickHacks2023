350ec3c628a48d8599ed54a99455d193f8ccd40a
Jorge <82967800+cantujorge17@users.noreply.github.com>
2023-04-15 14:03:15 -0500

    Made some progress on the first tab

--- pickhacks_project/src/pages/Tab1.tsx
+++ pickhacks_project/src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import {  IonItem, IonLabel } from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -7,17 +8,50 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>
+            <div className="ion-text-center">
+              CO2 Removal
+            </div>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">CO2 Removal</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <IonItem>
+          <IonLabel>
+            <div className="ion-text-wrap">
+              <div className="ion-text-center">
+                Planting trees is the focus of many environmental efforts, but few discuss the environmental
+                impact of planting techniques. Many species of trees store little carbon and some live too short to 
+                make a difference. With the CUFR tree carbon calculator provided by the department of 
+                agriculture, you can evaluate the impact of each tree you plant.
+              </div>
+            </div>
+          </IonLabel>
+        </IonItem>
+
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>--Card Title--</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>I made a paragraph</p>
+            <p>Graphic design is my passion</p>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
+
+      <IonFooter className='ion-padding'>
+        test
+      </IonFooter>
+
     </IonPage>
   );
 };
